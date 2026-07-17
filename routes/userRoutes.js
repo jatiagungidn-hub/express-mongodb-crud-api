65e8ce29474abbfffc843cd1eae536a2c0b88763
Jati Agung <jatiagung.idn@gmail.com>
2026-07-17 21:14:23 +0700

    Add dotenv module, fix initial folder, and add GET router

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const User = require("../models/User");
 
-const router = express.Router;
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({ count: users.length, data: users });
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require("./app");
 const connectDB = require("./config/db");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
-  await connectDB;
+  await connectDB();
 
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
